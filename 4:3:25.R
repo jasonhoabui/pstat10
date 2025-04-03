450f1ff003321dbfc49e7574035a8c6b446a1966
Jason Bui <jasonbui@169-231-65-42.wireless.ucsb.edu>
2025-04-03 10:18:26 -0700

    lab 2

---
+++ 4:3:25.R
@@ -0,0 +1,26 @@
+# question 1
+x <- c(5, 7, 3, 10, 1, 7, 19 ,5, 2)
+x[4]
+x[5:7]
+median(x)
+length(x)
+
+# question 2
+y <- c(rep(6, 7))
+y
+z <- c(rep(3, 3))
+z
+
+# question 3
+p <- c(2,3,4,6,6)
+q <- c(7,2,4,3,2)
+p+q
+sum(p+q)
+# p+q returns all the values in both vectors added to one vector
+# sum(p+q) returns all the values in both vectors added to one number
+
+# question 4
+x <- c(-50:-54, -53:-50)
+
+# question 5
+x <- c(rep(1, 2), rep(2, 2), rep(3, 1))
